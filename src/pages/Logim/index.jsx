3ca1185d1e1d9c031ff6c27fa2dd0ee222c88f39
joa242312 <joaogabrielss2006@gmail.com>
2025-05-02 11:52:19 -0300

    continuando sitema 03

--- src/pages/Logim/index.jsx
+++ src/pages/Logim/index.jsx
@@ -1,5 +1,20 @@
+
+import { Link,  useNavigate } from "react-router-dom"
+import animada from"/public/login-animate.svg"
 export default function LoginPage(){
+    const navigate = useNavigate()
     return(
-    <p>Aqui é a pagina de Logim</p>
+        <div className="w-full h-screen flex ">
+            <div className="w-[40%] h-full flex items-center justify-center">
+                 {/* <Link to="/Cadastro" >Casdastrar 1</Link>  */}
+                {/* <button onClick={() => navigate("/cadastro")}>Casdastrar 2</button> */}
+                <button onClick={() => window.location.href = "/Cadastro"}>Casdastrar</button>
+            </div>
+            <div className="w-[60%] h-full bg-[#6262eb] flex flex-col items-center justify-center rounded-lg" >
+                <img  width={600} src={animada} alt="imagem animada"/>
+                <p className="text-white font-bold w-[300px] text-[20px] text-center">  A melhor experiancia de longim que você já teve na sua vida</p>
+            </div>
+        </div>
+
     )
 }
